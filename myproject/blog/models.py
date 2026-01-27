a03d74ea9e78539911362a81dd877432ebfb1f22
NIKHILis-Coder <nrtambari@gmail.com>
2026-01-28 01:37:49 +0530

    Fix read-later redirect and improve navbar spacing

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
 class Tag(models.Model):
@@ -19,19 +19,24 @@ class Author(models.Model):
 class Posts(models.Model):
     title = models.CharField(max_length = 100)
     excerpt = models.CharField(max_length = 200)
-    content = models.TextField()
+    content = models.TextField(validators = [MinLengthValidator(10)])
     date = models.DateField(auto_now_add=True)
     image = models.ImageField(upload_to = "posts", null = True)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL ,related_name="posts" , null = True)
     slug = models.SlugField(unique=True, db_index=True)
     tag = models.ManyToManyField(Tag)
     
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     user_name = models.CharField(max_length=200)
     user_email = models.EmailField()
     text = models.TextField(max_length=400)
     post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name= "comments")
+    
+    def __str__(self):
+        return f"Comment by {self.user_name} on {self.post}"
     
 
     
--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -3,5 +3,6 @@ from .import views
 urlpatterns = [
     path("", views.starting_pageView.as_view(), name="starting-page"),
     path("posts", views.postsView.as_view(), name="post-page"),
-    path("posts/<slug:slug>/", views.post_detailView.as_view(), name="post-detail-page")
+    path("posts/<slug:slug>/", views.post_detailView.as_view(), name="post-detail-page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
